67f4073199ca23d2b1a4999b4cf68f901dcbc194
Madsen <chmad18@student.sdu.dk>
2021-03-06 18:44:50 +0100

    setup heroku postrgres db

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const imageSubmitter = require('./controllers/imageSubmitter');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'test',
-        database: 'smartbrain'
+        host: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
